21b9aac148b7458e71895c84ed49e5f09d6cdc56
Agustín Stringa <stringaagustin1@gmail.com>
2024-06-07 11:07:53 -0300

    feat:creating login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginComponent } from './login/login.component.js';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-gimnasio';
