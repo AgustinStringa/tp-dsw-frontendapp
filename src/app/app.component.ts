c5664491281c6f8f31bc12ca5c73b5b2df686157
Agustín Stringa <stringaagustin1@gmail.com>
2024-08-05 23:31:41 -0300

    initial templates
    Co-Authored-By: AaronDeBernardo <142444363+AaronDeBernardo@users.noreply.github.com>
    Co-Authored-By: Elias Tomas Danteo <163892255+EliasDanteo@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component.js';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FooterComponent } from './footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [
+    RouterOutlet,
+    LoginComponent,
+    MatSidenavModule,
+    FooterComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
