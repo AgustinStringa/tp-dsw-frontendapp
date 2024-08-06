eea3cf462f02a9bfc9538d37f5132cfed57251ba
Elias <elias.danteo.tomas@hotmail.com>
2024-08-06 14:05:16 -0300

    fix: login component: label covering input

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = ''; // 
+  password: string = ''; 
+
+  isEmailEmpty(): boolean {
+    return this.email.length === 0;
+  }
+  isPasswordEmpty(): boolean {
+    return this.password.length === 0;
+  }
+
 
 }
