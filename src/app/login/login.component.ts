df2c71c9f32bfdbc4d79f433895e1ee0d5b8175b
AaronDeBernardo <142444363+AaronDeBernardo@users.noreply.github.com>
2024-08-15 20:41:00 -0300

    feat: dinamically change

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   email: string = ''; //
   password: string = '';
+  isLoginVisible: boolean = true;
 
   isEmailEmpty(): boolean {
     return this.email.length === 0;
