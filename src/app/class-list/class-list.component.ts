add7073f0e1563b7132b11007c0b8ba3751f6737
Elias <elias.danteo.tomas@hotmail.com>
2024-08-23 17:00:30 -0300

    feat: show classes of classType selected

--- src/app/class-list/class-list.component.ts
+++ src/app/class-list/class-list.component.ts
@@ -2,6 +2,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ClassType } from '../core/interfaces/classType.interface.js';
+import { User } from '../core/interfaces/user.interface.js';
 
 @Component({
   selector: 'app-class-list',
@@ -11,20 +12,60 @@ import { ClassType } from '../core/interfaces/classType.interface.js';
   styleUrl: './class-list.component.css',
 })
 export class ClassListComponent {
-  url: string = '';
+  urlClass: string = '';
   classtype: ClassType[] = [];
+  selectedClassType: ClassType | null = null;
+  urlClassType: string = 'http://localhost:3000/api/classes/types';
+  urlTrainer: string = 'http://localhost:3000/api/trainers'; // URL para obtener trainers
+  trainers: { [id: string]: User } = {}; // Mapa para almacenar trainers por ID
 
   constructor(private http: HttpClient) {
-    this.url = 'http://localhost:3000/api/classes/types';
+    this.getClassTypes();
+  }
+  /*
+  async loadTrainers() {
+    try {
+      const response = await this.http.get<any>(this.urlTrainer).toPromise();
+      response.data.forEach((trainer: User) => {
+        this.trainers[trainer.id] = trainer;
+      });
+    } catch (error) {
+      console.error('Error loading trainers:', error);
+    }
   }
 
+  trackByClassId(index: number, item: any): string {
+    return item.id;
+  }
+  async getTrainerName(trainerId: string): Promise<string> {
+    try {
+      const response = await this.trainers[trainerId]?.firstName;
+      return response;
+    } catch (error) {
+      return 'Unknown Trainer';
+    }
+  }
+*/
   async getClassTypes() {
     try {
-      this.http.get<any>(this.url).subscribe((res) => {
+      this.http.get<any>(this.urlClassType).subscribe((res) => {
         this.classtype = res.data;
       });
     } catch (error: any) {
       console.log(error);
+    }
+  }
+
+  selectClassType(classType: ClassType) {
+    this.selectedClassType = classType;
+  }
+
+  getClassTypeInfo() {
+    if (this.selectedClassType) {
+      console.log('Selected Class Type:', this.selectedClassType);
+      // TO DO
+    } else {
+      console.log('No Class Type selected');
     }
   }
 }
--- src/app/core/interfaces/classType.interface.ts
+++ src/app/core/interfaces/classType.interface.ts
@@ -1,7 +1,18 @@
+export interface Class {
+  id: string;
+  day: number;
+  startTime: string;
+  endTime: string;
+  maxCapacity: number;
+  location: string;
+  active: boolean;
+  classType: string;
+  trainer: string;
+}
+
 export interface ClassType {
+  id: string;
   name: string;
-
   description: string;
-
-  classes: Object[];
+  classes: Class[];
 }
