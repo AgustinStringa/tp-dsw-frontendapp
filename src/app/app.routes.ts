a7cb2e87f20888e2a91e9769849aa6e2c88b328c
franciscag714 <franciscagramaglia714@gmail.com>
2024-09-05 18:40:52 -0300

    feat: membership-types-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { ClientsListComponent } from './clients-list/clients-list.component.js';
 import { TrainersListComponent } from './trainers-list/trainers-list.component.js';
 import { LoginComponent } from './login/login.component.js';
+import { MembershipTypesListComponent } from './membership-types-list/membership-types-list.component.js';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'clients', component: ClientsListComponent },
   { path: 'trainers', component: TrainersListComponent },
+  {
+    path: 'membershiptypes',
+    component: MembershipTypesListComponent,
+  },
 ];
---
+++ src/app/core/interfaces/current-membership.ts
@@ -0,0 +1,2 @@
+export interface CurrentMembership {
+}
