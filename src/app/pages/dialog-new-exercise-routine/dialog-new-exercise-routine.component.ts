c75979a316ac8af72807e4b60a7b9433cf021c4b
Agustín Stringa <stringaagustin1@gmail.com>
2024-09-11 00:35:07 -0300

    feat: dialog new exercise routine component created
    dialog used by create routine page component

---
+++ src/app/pages/dialog-new-exercise-routine/dialog-new-exercise-routine.component.ts
@@ -0,0 +1,61 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormsModule,
+  Validators,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DialogData } from '../create-routine-page/create-routine-page.component.js';
+import Exercise from '../../core/interfaces/exercise.interface.js';
+
+@Component({
+  selector: 'app-dialog-new-exercise-routine',
+  standalone: true,
+  imports: [
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogTitle,
+    MatDialogActions,
+    MatDialogClose,
+    FormsModule,
+    MatDialogContent,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './dialog-new-exercise-routine.component.html',
+  styleUrl: './dialog-new-exercise-routine.component.css',
+})
+export class DialogNewExerciseRoutineComponent {
+  readonly dialogRef = inject(MatDialogRef<DialogNewExerciseRoutineComponent>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  exerciseRoutineForm = new FormGroup({
+    exercise: new FormControl<Exercise | null>(null, Validators.required),
+    series: new FormControl<number>(1, Validators.required),
+    reps: new FormControl<number>(1, Validators.required),
+  });
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  returnValues() {
+    return {
+      exerciseSelected: this.exerciseRoutineForm.value.exercise,
+      series: this.exerciseRoutineForm.value.series,
+      reps: this.exerciseRoutineForm.value.reps,
+    };
+  }
+}
