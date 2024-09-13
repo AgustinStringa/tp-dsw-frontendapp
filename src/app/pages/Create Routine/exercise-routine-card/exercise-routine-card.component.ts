9418e6de95e3e19eefea78909f931c0658220589
Agustín Stringa <stringaagustin1@gmail.com>
2024-09-13 13:13:47 -0300

    feat: initial exercise-routine-card component created

---
+++ src/app/pages/Create Routine/exercise-routine-card/exercise-routine-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import IExerciseRoutine from '../../../core/interfaces/IExerciseRoutine.inteface.js';
+@Component({
+  selector: 'app-exercise-routine-card',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule],
+  templateUrl: './exercise-routine-card.component.html',
+  styleUrl: './exercise-routine-card.component.css',
+})
+export class ExerciseRoutineCardComponent {
+  @Input() exerciseRoutine: IExerciseRoutine | null = null;
+}
