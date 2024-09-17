fb6dcb41b96dc6a56863e9cebd442052e3460014
Agustín Stringa <stringaagustin1@gmail.com>
2024-09-17 14:27:09 -0300

    feat: navbar component started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { NavbarComponent } from './navbar/navbar.component.js';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,6 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatToolbarModule,
     RouterLink,
+    NavbarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
