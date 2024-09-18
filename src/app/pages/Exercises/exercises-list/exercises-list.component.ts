d2f43daa8b3a5b3aaec994b1723f3582797516ed
Agustín Stringa <stringaagustin1@gmail.com>
2024-09-18 10:36:52 -0300

    feat: delete exercises from frontend

--- src/app/pages/Exercises/exercises-list/exercises-list.component.ts
+++ src/app/pages/Exercises/exercises-list/exercises-list.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { environment } from '../../../../environments/environment.js';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { ComponentType } from '@angular/cdk/portal';
 import IExercise from '../../../core/interfaces/IExercise.interface.js';
+import { DeleteDialogComponent } from '../../../delete-dialog/delete-dialog.component.js';
+import { environment } from '../../../../environments/environment.js';
 @Component({
   selector: 'app-exercises-list',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, MatIconModule],
   templateUrl: './exercises-list.component.html',
   styleUrl: './exercises-list.component.css',
 })
 export class ExercisesListComponent {
   exercises: IExercise[] | undefined = [];
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private dialog: MatDialog) {
     this.getExercises();
+  }
+
+  openDialog(dialog: ComponentType<unknown>, data: object): void {
+    const dialogRef = this.dialog.open(dialog, { data });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.getExercises();
+    });
   }
 
   getExercises() {
@@ -23,5 +35,17 @@ export class ExercisesListComponent {
     } catch (error: any) {
       console.log(error);
     }
+  }
+
+  editExercise(e: IExercise): void {
+    // this.openDialog()
+  }
+
+  removeExercise(id: string): void {
+    this.openDialog(DeleteDialogComponent, {
+      id: id,
+      entity: 'exercises',
+      title: 'Eliminar ejercicio',
+    });
   }
 }
