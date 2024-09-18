e9a55704d67f1a2060d3c7a9a3eb982b70659fb4
Agustín Stringa <stringaagustin1@gmail.com>
2024-09-18 10:36:36 -0300

    chore: api routes of several delete endpoints added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,4 +12,7 @@ export const environment = {
   clientsWithMembershipUrl:
     'http://localhost:3000/api/clients/membership-active',
   exercisesUrl: 'http://localhost:3000/api/routines/exercises',
+  deleteClientUrl: 'http://localhost:3000/api/clients',
+  deleteTrainerUrl: 'http://localhost:3000/api/trainers',
+  deleteExerciseUrl: 'http://localhost:3000/api/routines/exercises',
 };
