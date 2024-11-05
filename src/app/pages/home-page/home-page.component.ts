4632985ed9f483c3433494cf9664547a7cb10c37
Agustín Stringa <stringaagustin1@gmail.com>
2024-11-05 15:29:41 -0300

    feat: styles and content for home component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
-
+  constructor(private router: Router) {}
+  navigateRegistration() {
+    this.router.navigate(['/registration']);
+  }
+  navigateRecordExerciseExecution() {
+    this.router.navigate(['/record-exercise-execution']);
+  }
 }
