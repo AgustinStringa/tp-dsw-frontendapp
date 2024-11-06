265c1232a93b69bd153ce8673310d14262b93680
Agustín Stringa <stringaagustin1@gmail.com>
2024-11-06 20:07:49 -0300

    feat: data presentation for client home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service.js';
+import { environment } from '../../../environments/environment.js';
+import { IMembership } from '../../core/interfaces/membership.interface.js';
 
 @Component({
   selector: 'app-home-page',
@@ -7,4 +11,77 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  public userSignal = this.authService.userSignal;
+  //TODO: tipar interfaces goals, progresses
+  public goals: [] = [];
+  public progresses: [] = [];
+  public membership: IMembership | null = null;
+  public classes: [] = [];
+  public membershipDateTo: Date | null = null;
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+    authService.getUser();
+    if (this.userSignal()?.isClient) {
+      this.getProgresses();
+      this.getGoals();
+      this.getClasses();
+      this.getMembership();
+    }
+  }
+
+  getGoals(): void {
+    try {
+      this.http
+        .get(`${environment.goalsUrl}/client/${this.userSignal()?.id}`)
+        .subscribe({
+          next: (res: any) => {
+            this.goals = res.data;
+          },
+          error: (err) => {},
+        });
+    } catch (error) {}
+  }
+
+  getProgresses(): void {
+    try {
+      this.http
+        .get(`${environment.progressesUrl}/client/${this.userSignal()?.id}`)
+        .subscribe({
+          next: (res: any) => {
+            this.progresses = res.data;
+          },
+          error: (err) => {},
+        });
+    } catch (error) {}
+  }
+
+  getMembership(): void {
+    try {
+      this.http
+        .get(`${environment.membershipsUrl}/active/${this.userSignal()?.id}`)
+        .subscribe({
+          next: (res: any) => {
+            this.membership = res.data;
+            if (this.membership?.dateTo != null) {
+              this.membershipDateTo = new Date(this.membership.dateTo);
+            }
+          },
+          error: (err) => {},
+        });
+    } catch (error) {}
+  }
+
+  getClasses(): void {
+    try {
+      this.http
+        .get(`${environment.registrationUrl}/client/${this.userSignal()?.id}`)
+        .subscribe({
+          next: (res: any) => {
+            this.classes = res.data;
+          },
+          error: (err) => {},
+        });
+    } catch (error) {}
+  }
+}
