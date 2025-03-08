bfbb1acc12d6800e06698a8ad96ad857bf8af801
Agustín Stringa <stringaagustin1@gmail.com>
2025-03-08 14:52:48 -0300

    first test
    first test created including all necessary providers and modules.
    component creation and data sent tested.

--- src/app/pages/membership-crud/membership-dialog/membership-dialog.component.spec.ts
+++ src/app/pages/membership-crud/membership-dialog/membership-dialog.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MembershipDialogComponent } from './membership-dialog.component';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MembershipsDialogComponent', () => {
   let component: MembershipDialogComponent;
@@ -8,7 +15,29 @@ describe('MembershipsDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MembershipDialogComponent],
+      imports: [
+        MembershipDialogComponent,
+        HttpClientModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            title: 'string',
+            action: 'string',
+            membership: undefined,
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MembershipDialogComponent);
@@ -18,5 +47,13 @@ describe('MembershipsDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('action should not be falsy', () => {
+    expect(component.action).not.toBeFalsy();
+  });
+
+  it('action should be put or post', () => {
+    expect(component.action).toMatch(/^(put|post)$/);
   });
 });
--- src/app/pages/membership-crud/membership-list/membership-list.component.spec.ts
+++ src/app/pages/membership-crud/membership-list/membership-list.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MembershipListComponent } from './membership-list.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
-describe('MembershipsListComponent', () => {
+xdescribe('MembershipsListComponent', () => {
   let component: MembershipListComponent;
   let fixture: ComponentFixture<MembershipListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MembershipListComponent],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MembershipListComponent);
