b9becd52181551b5892ef1bac302e442d6a923b4
Agustín Stringa <stringaagustin1@gmail.com>
2025-03-09 18:08:37 -0300

    fix: classTypeDialogComponent test

--- src/app/pages/class-type-crud/class-type-dialog/class-type-dialog.component.spec.ts
+++ src/app/pages/class-type-crud/class-type-dialog/class-type-dialog.component.spec.ts
@@ -1,16 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ExerciseDialogComponent } from '../../Exercises/exercise-dialog/exercise-dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ClassTypeDialogComponent } from './class-type-dialog.component';
 
-describe('ExerciseDialogComponent', () => {
-  let component: ExerciseDialogComponent;
-  let fixture: ComponentFixture<ExerciseDialogComponent>;
+describe('ClassTypeDialogComponent', () => {
+  let component: ClassTypeDialogComponent;
+  let fixture: ComponentFixture<ClassTypeDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExerciseDialogComponent],
+      imports: [
+        ClassTypeDialogComponent,
+        HttpClientModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            title: 'string;',
+            action: 'string;',
+            classType: undefined,
+          },
+        },
+      ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ExerciseDialogComponent);
+    fixture = TestBed.createComponent(ClassTypeDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
