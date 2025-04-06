dfbedc1cc97f53b4fd87ee3e4009bca23d5c7675
Elias Tomas Danteo <elias.danteo.tomas@hotmail.com>
2025-04-06 19:26:38 -0300

    fix: payment-list ui improvement feat: filteredClientswithMembership

--- src/app/pages/create-routine/clients-membership-list/clients-membership-list.component.ts
+++ src/app/pages/create-routine/clients-membership-list/clients-membership-list.component.ts
@@ -7,12 +7,12 @@ import {
   signal,
   WritableSignal,
 } from '@angular/core';
+import Client from '../../../core/classes/client';
 import { es } from 'date-fns/locale';
 import { format } from 'date-fns';
-import { NgClass } from '@angular/common';
-import { SimpleChanges } from '@angular/core';
-import Client from '../../../core/classes/client';
+import { FormsModule } from '@angular/forms';
 import { IMembership } from '../../../core/interfaces/membership.interface';
+import { SimpleChanges } from '@angular/core';
 
 class ClientForRoutine extends Client {
   public selected: boolean;
@@ -31,7 +31,7 @@ class ClientForRoutine extends Client {
 @Component({
   selector: 'app-clients-membership-list',
   standalone: true,
-  imports: [NgClass],
+  imports: [FormsModule],
   templateUrl: './clients-membership-list.component.html',
   styleUrl: './clients-membership-list.component.css',
 })
@@ -39,7 +39,9 @@ export class ClientsMembershipListComponent implements OnChanges {
   @Input() clientList: Client[] = [];
   @Output() clientChange = new EventEmitter<Client | null>();
   clientForRoutineList: ClientForRoutine[] = [];
+  filteredClients: ClientForRoutine[] = [];
   selectedClient: WritableSignal<ClientForRoutine | null> = signal(null);
+  searchTerm = '';
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['clientList']) {
@@ -55,7 +57,23 @@ export class ClientsMembershipListComponent implements OnChanges {
         );
         this.clientForRoutineList.push(newClient);
       });
+      this.filteredClients = [...this.clientForRoutineList];
     }
+  }
+  filterClients(): void {
+    if (!this.searchTerm) {
+      this.filteredClients = [...this.clientForRoutineList];
+      return;
+    }
+
+    const term = this.searchTerm.toLowerCase().trim();
+
+    this.filteredClients = this.clientForRoutineList.filter((client) => {
+      const fullName = `${client.firstName.toLowerCase()} ${client.lastName.toLowerCase()}`;
+      const reverseName = `${client.lastName.toLowerCase()} ${client.firstName.toLowerCase()}`;
+
+      return fullName.includes(term) || reverseName.includes(term);
+    });
   }
 
   toggleSelected(c: ClientForRoutine) {
