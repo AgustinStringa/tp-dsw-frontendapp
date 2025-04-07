1e341a269ee35ba1bb3ef28296f722bdba6438d9
Agustín Stringa <stringaagustin1@gmail.com>
2025-04-07 20:21:55 -0300

    label fixed. filter now delete diacritic

--- src/app/pages/membership-crud/membership-filter/membership-filter.component.ts
+++ src/app/pages/membership-crud/membership-filter/membership-filter.component.ts
@@ -14,13 +14,13 @@ export class MembershipFilterComponent {
   @Output() filterMemberships = new EventEmitter<IMembership[]>();
   @Input() memberships: IMembership[] | null = [];
   private filteredMemberships: IMembership[] = [];
-  public showPayPending = false;
+  public showOnlyPayPending = false;
   public searchValue = '';
 
   clearFilters() {
     if (this.memberships) {
       this.searchValue = '';
-      this.showPayPending = false;
+      this.showOnlyPayPending = false;
       const defaultMemberships = [...this.memberships];
       this.filterMemberships.emit(defaultMemberships);
     }
@@ -30,27 +30,29 @@ export class MembershipFilterComponent {
     if (this.memberships) {
       this.filteredMemberships = this.memberships.filter(
         (m) =>
-          m.client.firstName
-            .toLowerCase()
-            .includes(this.searchValue.toLowerCase()) ||
-          m.client.lastName
-            .toLowerCase()
-            .includes(this.searchValue.toLowerCase())
+          this.deleteDiacritic(m.client.firstName.toLowerCase())
+            .concat(' ')
+            .concat(this.deleteDiacritic(m.client.lastName.toLowerCase()))
+            .match(this.deleteDiacritic(this.searchValue).toLowerCase()) ||
+          this.deleteDiacritic(m.client.lastName.toLowerCase())
+            .concat(' ')
+            .concat(this.deleteDiacritic(m.client.firstName.toLowerCase()))
+            .match(this.deleteDiacritic(this.searchValue).toLowerCase())
       );
     }
   }
 
   applyToggleFilter() {
     if (!this.memberships) return;
-    if (this.showPayPending) {
+    if (this.showOnlyPayPending) {
       this.filteredMemberships = this.filteredMemberships.filter(
         (m) => m.debt && m.debt > 0
       );
-    } else {
-      this.filteredMemberships = this.filteredMemberships.filter(
-        (m) => !m.debt || m.debt == 0
-      );
     }
+  }
+
+  deleteDiacritic(text: string) {
+    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   }
 
   applyFilters() {
